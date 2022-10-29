4482cee6bbc16aee632c5f876c10981afade9ec2
SOFTLY <116372539+softly2000@users.noreply.github.com>
2022-10-29 16:23:00 +0300

    Add files via upload

---
+++ 12-task/12-task.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+    string fio = "SulaimanovChyngyzTelekovich";
+    string str;
+    string name="";
+    //int pos = fio.find(str, 0);
+    
+    name.append(fio,10,3);
+    
+    cout << name << endl;
+    return 0;
+}
